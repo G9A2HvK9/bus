0b3d14cb29f9d1b4f3e522fb785c8b87832be1af
hendrik <g9a2hvk9@me.com>
2017-09-11 22:12:16 +0200

    Adds Jasmine Test FrameWork

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # bus
+
+A web-app that combines transport for London (TFL), Google maps, Google places and Wikipedia API to generate an geo-accurate map of scenic and historic bus routes in London.
+
+**G9A2HvK9**
---
+++ spec/route_spec.js
@@ -0,0 +1,14 @@
+var Route = require('../lib/route.js')
+var route
+
+describe('Route', function(){
+
+  beforeEach(function(){
+    route = new Route();
+  });
+
+  it("Should exist", function(){
+    expect()
+  });
+
+});
