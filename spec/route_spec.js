c5334f46661e01e9c69b13091f7ee79232a35592
hendrik <g9a2hvk9@me.com>
2017-09-11 23:31:06 +0200

    Adds transport for London API

--- spec/route_spec.js
+++ spec/route_spec.js
@@ -4,11 +4,15 @@ var route
 describe('Route', function(){
 
   beforeEach(function(){
-    route = new Route();
+    route = new Route('Kensington Highstreet', 'Finsburgy Park');
   });
 
-  it("Should exist", function(){
-    expect()
+  it('Should exist', function(){
+    expect(Route).toBeDefined(); // CAREFUL! Only truthy if route == new Route() on line 1 of this file
+    expect(route).toBeDefined(); // CAREFUL! Only defined correctly if route == new Route() on line 8 && Route == require('../lib/route.js')
   });
 
+  it('Should respond to API requests from TfL', function(){
+    expect(route.get()).not.toBe(null);
+  });
 });
